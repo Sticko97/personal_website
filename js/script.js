a3fdbfacfcfb420dfce3553e7dbb81c6323245bb
Sticko97 <sle23@my.bcit.ca>
2022-11-11 22:19:26 -0800

    javascript prompt for code lab7

--- js/script.js
+++ js/script.js
@@ -2,17 +2,26 @@ const courseList = [
     {code:"ACIT 1630", name:"Math"},
     {code:"ACIT 1420", name:"Systems Admin"},
     {code:"ACIT 1630", name:"Database Administration"},
-
+    
 ]
 
-let input = ""
-do{
-    input = prompt("Enter a BCIT course code (4-digit number): ");
+let getInput = prompt("Enter a BCIT course code (4-digit number): ?")
+
+if(isNaN(getInput) || getInput.length !== 4){
+    getInput = prompt("Invalid input. Please enter again")
 }
-while(input.length !=4 || isNaN(Number(input)))
 
-let input = courseList.find(code => courseList.code === "1630", "1420", "1630");
+let hasName = false;
+for(course of courseList){
+    // console.log(course);
+    if(course.code.includes(getInput)){
+        console.log('Yes I am taking the course: ', course.code, " - ", course.name)
+        hasName = true;
+    }
+}
 
-// for(code in courseList){
-//     console.log(code)
-// }
+if(!hasName){
+    courseList.push({code:getInput, name: null})
+}
+console.log(courseList)
+
